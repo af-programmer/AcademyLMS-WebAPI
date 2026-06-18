bd5782cb5db8eb9bfd78b4b72cbeb12291711a45
af-programmer <f5821088@gmail.com>
2026-06-18 15:46:57 +0300

    feat: implement AcademyDbContext with fluent API configuration and connection string

--- AcademyLMS.API/Program.cs
+++ AcademyLMS.API/Program.cs
@@ -1,6 +1,12 @@
+using AcademyLMS.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AcademyDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AcademyDb")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
--- AcademyLMS.DataAccess/Entities/Enrollment.cs
+++ AcademyLMS.DataAccess/Entities/Enrollment.cs
@@ -1,17 +1,11 @@
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AcademyLMS.DataAccess.Entities;
 
 public class Enrollment
 {
-    [Key]
-    public int EnrollmentId { get; set; }
-
-    [Required]
     public int StudentId { get; set; }
 
-    [Required]
     public int CourseId { get; set; }
 
     [Required]
@@ -20,9 +14,7 @@ public class Enrollment
     [Required]
     public DateTime EnrollmentDate { get; set; }
 
-    [ForeignKey(nameof(StudentId))]
     public Student Student { get; set; } = null!;
 
-    [ForeignKey(nameof(CourseId))]
     public Course Course { get; set; } = null!;
 }
